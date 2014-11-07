8a3073cb957ede87df1f36a01ee444b25b444b8b
Namec <bruene@mathematik.uni-marburg.de>
2014-11-07 19:35:15 +0100

    Die Zucht Klasse

---
+++ script/breed.js
@@ -0,0 +1,10 @@
+function breed(user,startPop,goal){
+	this.user = user;
+	this.startPop = startPop;
+	this.goal = goal;
+	
+	var population=[];
+	for(i=0,i<population.length,i++){
+		population[i]= startPop.split(",")
+	}
+}
