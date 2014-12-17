4cf423d913ff0c0c0b8af48e8e7ac5211d25af0a
Julian <JulHeeger@gmail.com>
2014-12-17 21:05:49 +0100

    Add new Databaslayout

--- documentation/db/MouseBreed.sql
+++ documentation/db/MouseBreed.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 22. Nov 2014 um 14:32
+-- Erstellungszeit: 17. Dez 2014 um 21:02
 -- Server Version: 5.6.20
 -- PHP-Version: 5.5.15
 
@@ -23,10 +23,22 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `mousebreed_breed`
+-- Tabellenstruktur für Tabelle `aspiration`
 --
 
-CREATE TABLE IF NOT EXISTS `mousebreed_breed` (
+CREATE TABLE IF NOT EXISTS `aspiration` (
+`id` int(11) NOT NULL,
+  `number_of_mouse` int(11) NOT NULL,
+  `end_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `breed`
+--
+
+CREATE TABLE IF NOT EXISTS `breed` (
 `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `time_of_creation` date NOT NULL,
@@ -37,10 +49,10 @@ CREATE TABLE IF NOT EXISTS `mousebreed_breed` (
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `mousebreed_cage`
+-- Tabellenstruktur für Tabelle `cage`
 --
 
-CREATE TABLE IF NOT EXISTS `mousebreed_cage` (
+CREATE TABLE IF NOT EXISTS `cage` (
 `id` int(11) NOT NULL,
   `max_number_of_mouses` int(11) NOT NULL,
   `breed_id` int(11) NOT NULL
@@ -49,10 +61,10 @@ CREATE TABLE IF NOT EXISTS `mousebreed_cage` (
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `mousebreed_mouse`
+-- Tabellenstruktur für Tabelle `mouse`
 --
 
-CREATE TABLE IF NOT EXISTS `mousebreed_mouse` (
+CREATE TABLE IF NOT EXISTS `mouse` (
 `id` int(11) NOT NULL,
   `cage_id` int(11) NOT NULL,
   `breed_id` int(11) NOT NULL,
@@ -62,92 +74,80 @@ CREATE TABLE IF NOT EXISTS `mousebreed_mouse` (
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `mousebreed_User`
+-- Tabellenstruktur für Tabelle `user`
 --
 
-CREATE TABLE IF NOT EXISTS `mousebreed_User` (
+CREATE TABLE IF NOT EXISTS `user` (
 `id` int(11) NOT NULL,
   `user` varchar(30) COLLATE utf8_general_mysql500_ci NOT NULL,
   `password` varchar(300) COLLATE utf8_general_mysql500_ci NOT NULL,
   `firstname` varchar(30) COLLATE utf8_general_mysql500_ci NOT NULL,
   `lastname` varchar(30) COLLATE utf8_general_mysql500_ci NOT NULL,
   `email` varchar(50) COLLATE utf8_general_mysql500_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `mousebreed_Ziel`
---
-
-CREATE TABLE IF NOT EXISTS `mousebreed_Ziel` (
-`id` int(11) NOT NULL,
-  `number_of_mouse` int(11) NOT NULL,
-  `end_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci AUTO_INCREMENT=1 ;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `mousebreed_breed`
+-- Indexes for table `aspiration`
 --
-ALTER TABLE `mousebreed_breed`
+ALTER TABLE `aspiration`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `breed`
+--
+ALTER TABLE `breed`
  ADD PRIMARY KEY (`id`), ADD KEY `id` (`id`), ADD KEY `id_2` (`id`);
 
 --
--- Indexes for table `mousebreed_cage`
+-- Indexes for table `cage`
 --
-ALTER TABLE `mousebreed_cage`
+ALTER TABLE `cage`
  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `mousebreed_mouse`
+-- Indexes for table `mouse`
 --
-ALTER TABLE `mousebreed_mouse`
+ALTER TABLE `mouse`
  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `mousebreed_User`
+-- Indexes for table `user`
 --
-ALTER TABLE `mousebreed_User`
+ALTER TABLE `user`
  ADD PRIMARY KEY (`id`), ADD KEY `id` (`id`);
-
---
--- Indexes for table `mousebreed_Ziel`
---
-ALTER TABLE `mousebreed_Ziel`
- ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `mousebreed_breed`
+-- AUTO_INCREMENT for table `aspiration`
 --
-ALTER TABLE `mousebreed_breed`
+ALTER TABLE `aspiration`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `mousebreed_cage`
+-- AUTO_INCREMENT for table `breed`
 --
-ALTER TABLE `mousebreed_cage`
+ALTER TABLE `breed`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `mousebreed_mouse`
+-- AUTO_INCREMENT for table `cage`
 --
-ALTER TABLE `mousebreed_mouse`
+ALTER TABLE `cage`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `mousebreed_User`
+-- AUTO_INCREMENT for table `mouse`
 --
-ALTER TABLE `mousebreed_User`
+ALTER TABLE `mouse`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `mousebreed_Ziel`
+-- AUTO_INCREMENT for table `user`
 --
-ALTER TABLE `mousebreed_Ziel`
+ALTER TABLE `user`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
