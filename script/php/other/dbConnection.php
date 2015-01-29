b5d100229ede3130e53d9b6b5c06c66b2fdbd457
Julian <JulHeeger@gmail.com>
2015-01-29 11:06:45 +0100

    Datenbank connection

--- script/php/other/dbConnection.php
+++ script/php/other/dbConnection.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = new mysqli("localhost", "root", "database", "praedictum");
+$db = new mysqli("localhost", "Mouse", "MouseBreed", "breed");
 $stmt = "SET NAMES 'utf8'";
 $stmt = $db->prepare($stmt);
 $stmt->execute();
