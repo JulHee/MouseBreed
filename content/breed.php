e36d99b633371bb0f6ce260c9f2fc5eb8719c991
Julian <JulHeeger@gmail.com>
2015-02-04 14:13:17 +0100

    Fade in/out

--- content/breed.php
+++ content/breed.php
@@ -6,12 +6,31 @@
 </div>
 <div id="mid_content"class="tile_bot cont">      
     <div id="main" class="tile">Main
+            <label>Information: <input id="name" class="tile"></input></label>
+    <label>Art: 
+        <select id="optionlist">
+          <option value="success">Erfolg</option>
+          <option value="info">Information</option>
+          <option value="warn">Warnung</option>
+          <option value="error">Fehler</option>
+        </select>
+    </label>
+    <button id="mybutton">Hinzufügen</button>
         </div>
+</div>
+<div id="benachrichtigungen" class="benbox">
+	<div id="benliste_top">
+		<ul id="Benliste">
+        	
+    	</ul>
+    </div>
+    <div id="benliste_bot">
+    	<button id="deleteall">Alles Löschen</button>
+    </div>
 </div>
     <!--<<div id="zuchtziel" class="tile">Zuchtziel
         </div>
-    <div id="benachrichtigungen" class="tile">Benachrichtigungen
-        </div>         
+         
     <div id="bottom" class="tile new">
         <button id="bnt1" class="testButton" type="button">Click Me!</button>
         <button id="bnt2" class="testButton" type="button">Click Me!</button>
@@ -21,7 +40,7 @@
 <div id="MenuContainer">
     <ul id="navigation">
         <li class="ben">
-            <img src="/data/img/mail.png" width="30" height="30"/>
+            <img id="mail_pic" src="/data/img/mail.png" width="30" height="30" />
         </li>
         <li class="contact">
             <img src="/data/img/target.png" width="30" height="30"/>
--- script/js/content/breed.js
+++ script/js/content/breed.js
@@ -1,9 +1,69 @@
 $(document).ready(function() {
-
-  	$(".testButton").click(function() {
-    	$.notify("Eine Maus ist schwanger ","warn");
+	$("#mybutton").click(
+		function(){
+			addBen($("#name").val(),$("#optionlist").val());
 	});
-
+	$(".testButton").click(function() {
+		$.notify("Eine Maus ist schwanger ","warn");
+	});
+	$("#mail_pic").click(fadeInOut());
 });
+
+function addBen(nachricht,art)
+{
+  switch (art) {
+        case "success":
+          $.notify(nachricht,"success");
+            break;
+        case "warn":
+           $.notify(nachricht,"warn");
+            break;
+        case "info":
+           $.notify(nachricht,"info");
+            break;
+        case "error":
+           $.notify(nachricht,"error");
+            break;
+        default:
+           $.notify(nachricht,"info");
+            break;
+      }
+    $("#Benliste").append($('<li>').append(nachricht+"  ").append($('<a>').attr("onClick","$(this).parent().remove()").append("X")));
+}
+
+function fadeInOut(){
+	if ($("#benachrichtigungen").css('opacity') == 0){
+		unfade($("#benachrichtigungen"));
+	} else {
+		fade($("#benachrichtigungen"));
+	}
+}
+
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
+
+function unfade(element) {
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+        alert("here");
+    }, 10);
+}
 
 
