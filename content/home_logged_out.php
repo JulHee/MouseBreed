d88b12a887c4e9a3e164f4816e507d2ffb705e76
Jörn Kabuth <joern.kabuth@gmx.net>
2015-03-08 18:34:23 +0100

    Backround Jumbo

--- content/home_logged_out.php
+++ content/home_logged_out.php
@@ -1,7 +1,10 @@
-<div class="jumbotron">
-    <h1>Mäusezucht</h1>
+<div class="container-full-bg" style="background-image:url('//placehold.it/1024x700/449955/FFF');">
+    <div class="container special">
 
-    <p class="lead">
+        <div class="jumbotron ">
+        <h1>Mäusezucht</h1>
+
+        <p class="lead">
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
         dolore magna aliquyam erat, sed diam voluptua.
         At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
@@ -10,10 +13,12 @@
         diam voluptua.
         At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
         Lorem ipsum dolor sit amet.
-    </p>
+        </p>
 
-    <p><a class="btn btn-lg btn-success" href="login" role="button">Anmelden</a>
-        <a href="register" class="btn btn-lg btn-primary">Registrieren</a>
-    </p>
+        <p><a class="btn btn-lg btn-success" href="login" role="button">Anmelden</a>
+             <a href="register" class="btn btn-lg btn-primary">Registrieren</a>
+        </p>
+        </div>
+
+    </div>
 </div>
-
