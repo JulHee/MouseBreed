3e1959756a9199cb163dc032f579b57a8008c941
Julian <Julheeger@gmail.com>
2015-03-09 19:42:01 +0100

    Carousel

--- content/home_logged_in.php
+++ content/home_logged_in.php
@@ -1,1 +1,61 @@
 <h1 class="page-header">Willkommen</h1>
+<div class="col-md-12">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <img src="http://placehold.it/900x500" alt="...">
+
+                        <div class="carousel-caption">
+                            <h3>Willkommen Fremder</h3>
+
+                            <p>Jetzt auch mit Bugs</p>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="http://placehold.it/900x500" alt="...">
+
+                        <div class="carousel-caption">
+                            <h3>Folie 2</h3>
+
+                            <p>und Features</p>
+                            <button type="button" id="sidebarNextDay"
+                                    class="pull-right btn btn-info"><span class="glyphicon glyphicon-time"
+                                                                          aria-hidden="true"></span> Nächter Tag
+                            </button>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="http://placehold.it/900x500" alt="...">
+
+                        <div class="carousel-caption">
+                            <h3>Folie 3</h3>
+
+                            <p>und mehr Katzen</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Controls -->
+                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
