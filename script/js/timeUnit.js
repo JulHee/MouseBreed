0a9e1362ad6ab54e13bbcf4ed5dffc02fb9eb60c
Namec <bruene@mathematik.uni-marburg.de>
2015-03-10 13:50:55 +0100

    geschlechtsreife funktion in der Mouse Klasse

--- script/js/timeUnit.js
+++ script/js/timeUnit.js
@@ -10,14 +10,3 @@ function gainWeight(mouseArray){
         mouseArray[i].weight += addedWeight;
     };
 }
-/*
-* */
-function checkPubescent(mouseArray){
-    for ( var i = 0, l = mouseArray.length; i < l; i++ ) {
-        if(mouseArray[i].age >69){
-            mouseArray[i].pubescent = true;
-        }else{
-            mouseArray[i].pubescent = false;
-        };
-    };
-}
