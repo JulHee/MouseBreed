8c43a0f2ecf1f38cc54f0a02b9c22fa8028baf2e
hanebutt <christoph.hanebutt@gmail.com>
2015-03-11 13:37:13 +0100

    fix

--- content/breed.php
+++ content/breed.php
@@ -1,5 +1,3 @@
-<?php echo $_SESSION['selectedCage']; ?>
-
 <h1 class="page-header"><?php echo $_SESSION['loadedBreed']['name']; ?>
     <button type="button" id="sidebarNextDay"
             class="pull-right btn btn-info"><span class="glyphicon glyphicon-time"
