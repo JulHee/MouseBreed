309b259a19cfbfc7ada878ea916d7bf2fa8cddf8
Julian <Julheeger@gmail.com>
2015-03-29 01:27:04 +0100

    Unnötige Dateien entfernt.

--- script/js/content/newplay.js
+++ script/js/content/newplay.js
@@ -1,9 +1,7 @@
 // Das Canvas
 var stage;
-
 // Array das alle Mause enthält
 var arrMouse = [];
-
 // Der aktuelle Käfig
 var selectedCage = 1;
 
@@ -16,27 +14,20 @@ function Mouse(id) {
     this.mousecontrainer = null;
     this.mouselabel = null;
 }
-
 // Initialisieren der Informatioenen
-Mouse.prototype.init = function () {
-
+Mouse.prototype.init = function() {
     // Holen der Informationen zu der Maus aus dem LocalStorage
     var tmpInfoMouse = getInfo(this.id);
-
     // Laden des Bilds
     this.mouseimg.src = this.src;
-
     // Konvertieren in ein createjs.Bitmap
     this.mouseani = new createjs.Bitmap(this.mouseimg);
-
     // Erstellen des Labels
     this.mouselabel = new createjs.Text("#" + tmpInfoMouse.id + ":" + tmpInfoMouse.name, "bold 14px Arial", "#314ddf");
     this.mouselabel.textAlign = "center";
-
     // Positionieren des Labels
     this.mouselabel.y = 35;
     this.mouselabel.x = 20;
-
     // Erstellen des Containers der das Label und das Bild beinhaltet
     this.mousecontrainer = new createjs.Container();
     this.mousecontrainer.addChild(this.mouseani, this.mouselabel);
@@ -47,21 +38,17 @@ Mouse.prototype.init = function () {
     this.mousecontrainer.vY = 2;
     this.mousecontrainer.x = 16 + Math.random() * 300;
     this.mousecontrainer.y = 32 + Math.random() * 300;
-
     // Erstellen der Hitbox. Achtung laut Docs püft hitTest() nicht auf Hitboxen
     var hit = new createjs.Shape();
     hit.graphics.beginFill("#000").drawRect(0, 0, this.mouseimg.width, this.mouseimg.height);
     this.mousecontrainer.hitArea = hit;
-
     // Registrieren der Events
-
     // Beim klicken laden der Informationen
-    this.mousecontrainer.on("click", function (elem) {
+    this.mousecontrainer.on("click", function(elem) {
         clickedMouse(elem.target.mouseid);
     });
-
     // Bei Maustastendruck (Drag-and-Drop). Später für Käfige
-    this.mousecontrainer.on("pressmove", function (evt) {
+    this.mousecontrainer.on("pressmove", function(evt) {
         console.log(evt.currentTarget);
         evt.currentTarget.ismove = false;
         // currentTarget will be the container that the event listener was added to:
@@ -70,9 +57,8 @@ Mouse.prototype.init = function () {
         // make sure to redraw the stage to show the change:
         stage.update();
     });
-
     // Beim loslasen der Maustaste
-    this.mousecontrainer.on("pressup", function (evt) {
+    this.mousecontrainer.on("pressup", function(evt) {
         evt.currentTarget.ismove = true;
     });
 };
@@ -96,7 +82,6 @@ function updateMouseArray(cageid) {
     var parsedData = JSON.parse(data);
     var thisCage = parsedData.cages;
     var thisMice = thisCage[cageid].mice;
-
     for (var key in thisMice) {
         var tmp = new Mouse(thisMice[key].id);
         tmp.init();
@@ -107,31 +92,25 @@ function updateMouseArray(cageid) {
 function init() {
     // Laden des Canvas
     stage = new createjs.Stage("demoCanvas");
-
     // Laden der Käfiginformationen
     updateMouseArray(1);
-
     // Hinzufügen aller Mäuse im Array in das Canvas
     for (var i = 0; i < arrMouse.length - 1; i++) {
         stage.addChild(arrMouse[i].mousecontrainer);
     }
-
     // Registrieren der Tick-Funktion als Zeitgeber
     createjs.Ticker.on("tick", tick);
     createjs.Ticker.timingMode = createjs.Ticker.RAF_SYNCHED;
     createjs.Ticker.setFPS(60);
-
     stage.update();
 }
+
 function tick(event) {
     for (var i = 0; i < arrMouse.length - 1; i++) {
-
         // Aktuelles Element
         var elem = arrMouse[i].mousecontrainer;
-
         // Umrechnen der Maus von Gobal nach Lokal
         var pt = elem.globalToLocal(stage.mouseX, stage.mouseY);
-
         // Wenn das Element nicht per Drag-and-Drop bewegt wird
         if (elem.ismove) {
             // Checken über welchem Element die Maus schwebt
@@ -150,13 +129,10 @@ function tick(event) {
             }
         }
     }
-
     // Aktualisieren des Canvas
     stage.update(event); // important!!
 }
-
-$(document).ready(function () {
-
+$(document).ready(function() {
     // Aufrufen der Startfunktion
     init();
 });
